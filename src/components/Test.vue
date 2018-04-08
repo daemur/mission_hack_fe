b013c562ff169965fdaefe0b51c8011438802413
Minh Anh Trinh <minhanh.xiu@gmail.com>
2018-04-08 12:30:53 -0400

    update last page

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="container">
-		Test route
+		<h1 class="title">Congratulations! It's finished!</h1>
+		<app-btn to-route="home" name="Find another recipe"></app-btn>
 		
 	</div>
 </template>
